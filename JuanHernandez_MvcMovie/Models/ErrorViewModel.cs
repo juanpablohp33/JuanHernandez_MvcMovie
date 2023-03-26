c99c3d6405729f0ea561895426dbd9baa229a4b1
Juan Pablo Hernandez Palacios <juanpablohp31@gmail.com>
2023-03-26 10:46:45 -0400

    Firts commit

---
+++ JuanHernandez_MvcMovie/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace JuanHernandez_MvcMovie.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
---
+++ JuanHernandez_MvcMovie/Models/Movie.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace JuanHernandez_MvcMovie.Models
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class Movie
+    {
+        private readonly RequestDelegate _next;
+
+        public Movie(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext httpContext)
+        {
+
+            return _next(httpContext);
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class MovieExtensions
+    {
+        public static IApplicationBuilder UseMiddlewareClassTemplate(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<Movie>();
+        }
+    }
+}
+
